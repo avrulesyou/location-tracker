8ab88be690188bcb42c665c287060527988aa76c
Abhishek Vishwakarma <avrulesyou@gmail.com>
2025-10-17 08:06:54 +0530

    Register services and configure logging in MauiProgram
    Added service registrations for DatabaseService and LocationService, and configured logging for debug.

--- LocationHeatmap/MauiProgram.cs
+++ LocationHeatmap/MauiProgram.cs
@@ -1,1 +1,30 @@
+using LocationHeatmap.Services;
+using Microsoft.Extensions.Logging;
 
+namespace LocationHeatmap;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiMaps()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        // Register services
+        builder.Services.AddSingleton<DatabaseService>();
+        builder.Services.AddSingleton<LocationService>();
+        builder.Services.AddTransient<MainPage>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
