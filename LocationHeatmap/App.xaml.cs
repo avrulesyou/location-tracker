ce47b698acf41d0a9a095d0f409ee2db5a8261c4
Abhishek Vishwakarma <avrulesyou@gmail.com>
2025-10-17 07:44:23 +0530

    Add App class with constructor for MainPage

---
+++ LocationHeatmap/App.xaml.cs
@@ -0,0 +1,11 @@
+namespace LocationHeatmap
+{
+    public partial class App : Application
+    {
+        public App(MainPage mainPage)
+        {
+            InitializeComponent();
+            MainPage = mainPage;
+        }
+    }
+}
